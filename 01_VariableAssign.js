5835928e74b66c3da72025d2267b31942f3134a1
Vashim Nadaf <vashimnadaf12@gmail.com>
2024-03-08 18:13:49 +0530

    Variable Assignment
    Variable Assignment

---
+++ 01_VariableAssign.js
@@ -0,0 +1,33 @@
+
+var myFullName="Vashim Nadaf";
+console.log("My Full Name before initialization",myFullName);
+
+
+myFullName="Vashim Badeso Nadaf";
+console.log("My Full Name After Update:",myFullName);
+
+console.log("======Step 2======");
+
+var myLove="I Love Only JS";
+console.log("My Love",myLove);
+
+console.log("======Step 3======");
+
+
+var firstName="Vashim";
+console.log("First Name:",firstName);
+
+var lastName="Nadaf";
+console.log("Last Name:",lastName);
+
+var address="Pune";
+console.log("Address:",address);
+
+var pinCode="416413";
+console.log("Pincode:",pinCode);
+
+var gender="Male";
+console.log("Gender:",gender);
+
+var maritalStatus="Unmarried";
+console.log("Marital Status:",maritalStatus);
