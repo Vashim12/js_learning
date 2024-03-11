e0137dee0e79e307a1098b58bc80cbe18d396ed8
Vashim Nadaf <vashimnadaf12@gmail.com>
2024-03-11 12:31:44 +0530

    Update Fun Assignment 01
    Update Fun Assignment 01

--- 03_funAssign01.js
+++ 03_funAssign01.js
@@ -40,17 +40,17 @@ swapValues("Virat","Anushka");
 
 console.log("********************************");
 
-function swapValues(arg1,arg2) {
+function swap(arg3,arg4) {
     console.log("===== Before Swap =====");
-    console.log("1000:",arg1,"2000:",arg2);
+    console.log("1000:",arg3,"2000:",arg4);
     console.log("===== After Swap =====");
-    var temp = arg1;
-    arg1 = arg2;
-    arg2 = temp;
-    console.log("1000:",arg1,"2000:",arg2);
+    var temp = arg3;
+    arg3 = arg4;
+    arg4 = temp;
+    console.log("1000:",arg3,"2000:",arg4);
         
     }
-    swapValues(1000,2000)
+    swap(1000,2000)
 
     console.log("========== Step - 4 ==========");
 
