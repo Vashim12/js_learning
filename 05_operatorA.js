5455f89c077fbe7ea021e0105eb149f8cc665e00
Vashim Nadaf <vashimnadaf12@gmail.com>
2024-03-14 13:26:34 +0530

    Operator Assignment
    Operator Assignment

---
+++ 05_operatorA.js
@@ -0,0 +1,34 @@
+console.log(`******** Step -1 ********`);
+function squareOfWordLength(back) {
+  
+  
+}
+squareOfWordLength()
+var back = "JavaScript";
+var result = back.length
+var result = 10**2
+console.log(`Length Square of Javascript: ${result}`);
+
+var back = "Google Chrome";
+var result = back.length
+var result = 13**2
+console.log(`Length Square of Google Chrome:${result}`);
+
+var back = "Developer Smart";
+ var result = back.length
+var result =15**2
+console.log(`Length Square of Developer Smart:${result}`); 
+
+console.log(`********** Step 2 *********`);
+
+function react() {
+    var devloper = "I am React Developer";
+    var result = devloper.length/4;
+    console.log(`String length after divde:${result}`);
+    
+    var result = devloper.length*4
+    console.log(`String length after multiply:${result}`);
+  
+}
+react();
+
