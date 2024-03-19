d45314fa9ee6358495911083f1db10ba2b1ac17c
Vashim Nadaf <vashimnadaf12@gmail.com>
2024-03-19 15:19:10 +0530

    For Loop Assignment
    For Loop Assignment

---
+++ 06_forLoop.js
@@ -0,0 +1,24 @@
+console.log(`// WAP to print numbers from 0 to 10`);
+for (let i = 0; i <=10; i++) {
+    console.log(i);
+    
+}
+console.log(` WAP to print numbers from 15 to 24`);
+for (let i = 15; i <=24; i++) {
+    console.log(i)}
+
+console.log(` WAP to print numbers from 10 to 5`);
+for (let i = 10; i <=5; i--) {
+    console.log(i);
+    
+}    
+
+console.log(` WAP to print even numbers from 0 to 20`);
+for (let index = 0; index <=20; index=index+2) {
+    console.log(index); 
+}
+
+console.log(` WAP to print odd numbers from 15 to 29`);
+for (let index = 15; index <=29; index=index+2) {
+    console.log(index); 
+}
---
+++ 06_for_Assignment.js
@@ -0,0 +1,48 @@
+console.log(`WAP to print numbers from to 5 to 15 by incrementing by 1`);
+
+for (let i = 5; i <=15; i++) {
+    console.log(i);
+    
+}
+
+console.log(`WAP to print numbers from to 50 to  by decrementing by 1`);
+
+for (let i = 50; i >=40; i--) {
+    console.log(i);
+    
+}
+
+console.log(`WAP to find first 15 odd numbers`);
+
+for (let i = 1; i <=30; i=i+2) {
+    console.log(i);
+    
+}
+
+console.log(`WAP to find 10 even numbers`);
+
+for (let i = 0; i <=20; i=i+2) {
+    console.log(i);
+    
+}
+
+console.log(`WAP to print table of 5 `);
+
+for (let i = 5; i <=50; i=i+5) {
+    console.log(i);
+    
+}
+
+console.log(`WAP to print table of 10`);
+
+for (let i = 10; i <=100; i=i+10) {
+    console.log(i);
+    
+}
+
+console.log(`WAP to print table of 10 reverse`);
+
+for (let i = 100; i>=10; i=i-10) {
+    console.log(i);
+    
+}
